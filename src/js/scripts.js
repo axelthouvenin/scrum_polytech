387ba976a15d9edb30145311ad7a731dc169f9d4
Flynet-BDD <77499285+Flynet-BDD@users.noreply.github.com>
2022-03-17 10:25:23 +0100

    Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,4 +4,24 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
 // This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Use this file to add JavaScript to your project 
+var number = 0;
+
+// Faire en sorte que lorsque l'utilisateur ajoute au panier il faut que par la suite dans la page panier on retrouve les items ajoutés
+var tableauImage = [];
+var tableauPrix = [];
+
+//Problème 1 : Au changement de page le fichier javascript se recharge cela supprime donc les éléments déjà enregistrés en JS
+//Solution 1 : 
+
+
+function increment()
+{
+    number++;
+    document.getElementById("nbrPanier").innerText = number;
+}
+
+function refreshpanier()
+{
+    document.getElementById("nbrPanier").innerText = number;   
+}
